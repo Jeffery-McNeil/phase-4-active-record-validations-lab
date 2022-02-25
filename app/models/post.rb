ad877f08687f7012a693aa6dc080fcdb3baa393e
Jeffery <jeffmc825@gmail.com>
2022-02-25 17:45:52 -0600

    commit tree 4f1bccd92616bcb27afe56970e23cbe20cf38cfa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait
+
+
+    def title_is_clickbait
+        unless title =~ /Won't Believe/ || /Secret/ || /Top [number]/ || /Guess/ 
+            errors.add(:title, "Title is not sufficiently clickbait-y")
+        end
+    end
 end
